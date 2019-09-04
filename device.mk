5d56b4acedb7f3cee5ae28f29d455c055b441d9e
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:50:41 -0400

    james: Setup device soong namespace
    Change-Id: I2e9c0ab68a0fe86ba18e3955e2823b87d88e835a
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -59,3 +59,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
