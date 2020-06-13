37e7e3ad6e2b15719677a6ee2fecd12ebea04f65
Jarl-Penguin <jarlpenguin@outlook.com>
2020-06-13 16:14:56 +0300

    james: Decommonize density prop
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ie763739e394f27a5483d48cdfae2ad5707992a63

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -2,3 +2,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.HAL3.enabled=1 \
     persist.vendor.camera.tof.direct=1
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=320
