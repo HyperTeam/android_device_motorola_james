25246364a8098ad47f48b49bd2c8b8217ef2d795
Jarl-Penguin <jarlpenguin@outlook.com>
2020-08-23 18:41:15 +0300

    james: We use common EGL blobs now
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -3,9 +3,3 @@
 SKU=`getprop ro.boot.hardware.sku`
 
 rm -rf /vendor/app/LineageActions
-mv /vendor/lib/egl/libEGL_adreno_8917.so /vendor/lib/egl/libEGL_adreno.so
-mv /vendor/lib/egl/libGLESv1_CM_adreno_8917.so /vendor/lib/egl/libGLESv1_CM_adreno.so
-mv /vendor/lib/egl/libGLESv2_adreno_8917.so /vendor/lib/egl/libGLESv2_adreno.so
-mv /vendor/lib64/egl/libEGL_adreno_8917.so /vendor/lib64/egl/libEGL_adreno.so
-mv /vendor/lib64/egl/libGLESv1_CM_adreno_8917.so /vendor/lib64/egl/libGLESv1_CM_adreno.so
-mv /vendor/lib64/egl/libGLESv2_adreno_8917.so /vendor/lib64/egl/libGLESv2_adreno.so
